97055cbc74902126df3043428766f67babe1d07d
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-07-26 11:03:47 +0300

    hello

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Nav from './Components/Nav'
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+        <Nav/>
+        <Header/>
+        <Main/>
+        <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href='/nav'></a></li>
+        <li><a href='/header'></a></li>
+        <li><a href='/main'></a></li>
+        <li><a href='/footer'></a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
