b1d8ecd8421b8e656aead5ec728e78eb2fd22d93
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-07-26 21:57:16 +0300

    hello

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import './Component-css/header.css'
+import hero from './icons_assets/restauranfood.jpg'
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className='header-grid-system'>
+      <div className='header-sec-one'>
+        <h1>Little Lemon</h1>
+        <h2>chicago</h2>
+        <p>we are a family owned Mediterranean restaurant focused on traditional recipes on traditional recipes served with a modern twist</p>
+        <a role='button'>Reserve a Table</a>
+      </div>
+      <div className='header-sec-two'>
+        <div className='header-img-container'>
+          <img src={hero}/>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import logo from './icons_assets/Logo.svg'
+import './Component-css/nav.css'
 const Nav = () => {
   return (
-    <nav>
-      <ul>
-        <li><a href='/nav'></a></li>
-        <li><a href='/header'></a></li>
-        <li><a href='/main'></a></li>
-        <li><a href='/footer'></a></li>
+    <nav className='nav-grid-system'>
+      <ul className='nav-links-container'>
+        <li className='nav-link nav-link-logo'><a href='/home'><img src={logo}/></a></li>
+        <li className='nav-link'><a href='/home'>Home</a></li>
+        <li className='nav-link'><a href='/about'>About</a></li>
+        <li className='nav-link'><a href='/menu'>Menu</a></li>
+        <li className='nav-link'><a href='/reservations'>Reservations</a></li>
+        <li className='nav-link'><a href='/order-online'>Order Online</a></li>
+        <li className='nav-link '><a href='/login'>Login</a></li>
       </ul>
     </nav>
   )
