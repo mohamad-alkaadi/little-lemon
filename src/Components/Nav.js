5f5d6371883707b05ae5ab0cbbf4a66d86e3326b
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-07-26 22:09:30 +0300

    hello

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,15 +4,17 @@ import './Component-css/nav.css'
 const Nav = () => {
   return (
     <nav className='nav-grid-system'>
-      <ul className='nav-links-container'>
-        <li className='nav-link nav-link-logo'><a href='/home'><img src={logo}/></a></li>
-        <li className='nav-link'><a href='/home'>Home</a></li>
-        <li className='nav-link'><a href='/about'>About</a></li>
-        <li className='nav-link'><a href='/menu'>Menu</a></li>
-        <li className='nav-link'><a href='/reservations'>Reservations</a></li>
-        <li className='nav-link'><a href='/order-online'>Order Online</a></li>
-        <li className='nav-link '><a href='/login'>Login</a></li>
-      </ul>
+      <div className='nav-container'>
+        <a href='/home' className='nav-link-logo'><img src={logo}/></a>
+        <ul className='nav-links-container'>
+          <li className='nav-link'><a href='/home'>Home</a></li>
+          <li className='nav-link'><a href='/about'>About</a></li>
+          <li className='nav-link'><a href='/menu'>Menu</a></li>
+          <li className='nav-link'><a href='/reservations'>Reservations</a></li>
+          <li className='nav-link'><a href='/order-online'>Order Online</a></li>
+          <li className='nav-link '><a href='/login'>Login</a></li>
+        </ul>
+      </div>
     </nav>
   )
 }
