7579c4cbf4b94dc5a03c498b15636db6ef990595
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-07-27 07:30:31 +0300

    hello

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import './Component-css/main.css'
+import { RiEBike2Fill} from 'react-icons/ri';
+
+import bruchetta from './icons_assets/bruchetta.svg'
+import lemon from './icons_assets/lemon-dessert.jpg'
+import greek from './icons_assets/greek-salad.jpg'
 
 const Main = () => {
   return (
-    <main>Main1</main>
+    <main className='main-grid-system'>
+      <div className='main-title-container'>
+        <p>This weeks specials!</p>
+        <a>Online Menu</a>
+      </div>
+      <div className='main-card-one main-card'>
+        <div>
+          <div className='card-img-container'>
+            <img src={greek}/>
+          </div>
+          <div className='main-card-titleAndPrice'>
+          <h3 className='main-card-title'>greek salad</h3>
+          <h3 className='main-card-price'>$12.99</h3>
+          </div>
+          <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, granished with crunchy garlic and rosemary croutons</p>
+          <a className='main-card-button'>Order a delivery {<RiEBike2Fill/>}</a>
+        </div>
+      </div>
+      <div className='main-card-two main-card'>
+        <div>
+          <div className='card-img-container'>
+            <img src={bruchetta}/>
+          </div>
+          <div className='main-card-titleAndPrice'>
+          <h3 className='main-card-title'>Bruchetta</h3>
+          <h3 className='main-card-price'>$5.99</h3>
+          </div>
+          <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and oliv oil.</p>
+          <a className='main-card-button'>Order a delivery {<RiEBike2Fill/>}</a>
+        </div>
+      </div>
+      <div className='main-card-three main-card'>
+        <div>
+          <div className='card-img-container'>
+            <img src={lemon}/>
+          </div>
+          <div className='main-card-titleAndPrice'>
+          <h3 className='main-card-title'>Lemon Dessert</h3>
+          <h3 className='main-card-price'>$5.00</h3>
+          </div>
+          <p>This comes straight from granma's recipe book. every last ingredient has been sourced and is as authentic as can be imagined</p>
+          <a className='main-card-button'>Order a delivery {<RiEBike2Fill/>}</a>
+        </div>
+      </div>
+    </main>
   )
 }
 
