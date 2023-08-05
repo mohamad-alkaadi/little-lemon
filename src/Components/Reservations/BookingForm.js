d4fc0598d15da6f2858070b9bd652c4d22b321d3
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-08-05 23:49:48 +0300

    ss

--- src/Components/Reservations/BookingForm.js
+++ src/Components/Reservations/BookingForm.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react'
 import './reservations-css/bookingForm.css'
-import { Link, useMatch, useResolvedPath, useHistory } from "react-router-dom"
+import { Link, useMatch, useResolvedPath, useNavigate } from "react-router-dom"
 
-
-const handleSubmit = (e) => {
-    e.preventDefault()
-    history.push('/bookingConformed')
-}
 
 const BookingForm = (props) => {
     const [date, setDate] = useState('')
     const [time, setTime] = useState('')
     const [guests, setGuests] = useState(1)
     const [occasion, setOccasion] = useState('Birthday')
-    
-    const history = useHistory()
+    const navigate = useNavigate()
+    const handleSubmit = (e) => {
+    e.preventDefault()
+    navigate('/bookingConformed')
+}
 
-//hellooooo
+
 
   return (
     <>
@@ -24,13 +22,13 @@ const BookingForm = (props) => {
             <label htmlFor='res-date' data-test-id='bookNow'>Choose date</label>
             <input type='date' id='res-date' value={date} onChange={(e) => setDate(e.target.value)}/>
             <label htmlFor="res-time">Choose time</label>
-            <select id="res-time " value={time} onChange={(e) => setTime(e.target.value)}>
+            <select id="res-time" value={time} onChange={(e) => setTime(e.target.value)}> // fix the typo
                 {props.availableTimes.map((t) => (
                     <option key={t}>{t}</option>
                 ))}
             </select>
             <label htmlFor="guests">Number of guests</label>
-            <input type='number' placeholder='1' min="1" max="10" id='guests'/>
+            <input type='number' placeholder='1' min="1" max="10" id='guests' value={guests} onChange={(e) => setGuests(e.target.value)}/> // use the state value and setter
             <label htmlFor="occasion">Occasion</label>
             <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
                 <option>Birthday</option>
@@ -43,7 +41,7 @@ const BookingForm = (props) => {
   )
 }
 
-
+// sdsds
 function CustomLink({ to, children, ...props }) {
     const resolvedPath = useResolvedPath(to)
     const isActive = useMatch({ path: resolvedPath.pathname, end: true })
@@ -56,4 +54,3 @@ function CustomLink({ to, children, ...props }) {
   }
   
 export default BookingForm
-
--- src/Components/Reservations/Reservations.js
+++ src/Components/Reservations/Reservations.js
@@ -1,46 +1,61 @@
-
 import React, {useEffect, useReducer, useState} from 'react'
 import BookingForm from './BookingForm'
 
 const CHANGE_DATE = 'change-date'
 
+const seededRandom = seed => {
+  const m = 2 ** 35 - 31;
+  const a = 185852;
+  let s = seed % m;
+  return () => (s = s * a % m) / m;
+};
+const fetchAPI = date => {
+  return new Promise((resolve, reject) => {
+    let result = [];
+    let random = seededRandom(date.getDate());
+    for(let i = 17; i <= 23; i++) {
+      if(random() < 0.5) result.push(i + ':00');
+      if(random() >= 0.5) result.push(i + ':30');
+    }
+    resolve(JSON.stringify(result))
+  })}
+  const submitAPI = formData => {
+    return new Promise((resolve, reject) => {
+      resolve(JSON.stringify(true))
+    })}
+    export {fetchAPI, submitAPI}
+
+
 const Reservations = () => {
 
+const sDate = new Date().toISOString().split('T')[0]
 
 const updateTimes = (state,action) =>{
   switch(action.type){
+    case 'initializeTimes':
+      return action.availableTimes // return a new array
     case CHANGE_DATE :
-      return state.filter(time => time.startsWith(action.date.slice(-1)))
+      return state.filter(time => time.startsWith(action.date.slice(-1))) // return a new array
     default:
       return state
   }
 }
 
+const [fData,setFData] = useState([]) // initialize with an empty array
 
-const{fData,setFData} = useState(null)
+useEffect(() => { 
+  fetchAPI(new Date(sDate))
+  .then(response => JSON.parse(response))
+  .then(availableTimes => setFData(availableTimes)) // use the setter function to update the state
+},[sDate]) // add sDate as a dependency
 
-useEffect(() => { fetch('https://raw.githubusercontent.com/Meta-Front-End-Developer-PC/capstone/master/api.js') 
-.then(res => {
-  if (res.ok) {
-    return res.json() 
-  } else {
-    throw new Error(`Something went wrong: ${res.status}`) } }) 
-    .then(data => {
-      setFData(data) })
-      .catch(error => {
-        console.error(error) }) },[])
+const [availableTimes, dispatch] = useReducer(updateTimes, fData) // use fData as the initial state
 
-
-const initializeTimes = () => { return fData }
-const [availableTimes, dispatch] = useReducer(updateTimes, null, initializeTimes)
-console.log(initializeTimes)
   return (
     <>
-      {initializeTimes && <inBookingForm availableTimes={availableTimes} onDateChange={(date) => dispatch({type: CHANGE_DATE, date})}/>}
+      {fData.length > 0 && <BookingForm availableTimes={availableTimes} onDateChange={(date) => dispatch({type: CHANGE_DATE, date})}/>}
     </>
   )
 }
-
-
 
 export default Reservations
